d500e9cc52a463ef915737f14502b4e770a7a039
Brad Wilson <brad.n.wilson@live.com>
2019-10-23 12:52:19 -0400

    Added styling and colors to suit  - Created /utils/Dom.js to isolate Dom functions

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
 const {
     default: { singleDeckGame }
   } = require("blackjack-dealer-logic");
+import Dom from './utils/Dom';
   
-  singleDeckGame.deal();
+singleDeckGame.deal();
+// Dom.generateCard(userHand.getCards()[0]),
+// Dom.generateCard(userHand.getCards()[1])
   
-  const userHand = singleDeckGame.getUserHand();
+const userHand = singleDeckGame.getUserHand();
+const dealerHand = singleDeckGame.getDealerHand();
+const dealtHand = {
+  userCards: userHand.getCards(),
+  dealerCards: dealerHand.getCards()
+}
+console.log(dealtHand);
+Dom.dealCards(dealtHand);
   
-  generateCard(userHand.getCards()[0]);
-  generateCard(userHand.getCards()[1]);
-  
-  function generateCard(card) {
-    const playingCard = document.createElement("section");
-    playingCard.classList.add("playing-card");
-  
-    const valueContainer = document.createElement("section");
-    valueContainer.classList.add("value-container");
-  
-    const value = document.createElement("span");
-    value.classList.add("value");
-    value.textContent = card.getValue();
-  
-    const suit = document.createElement("span");
-    suit.classList.add("suit");
-    suit.textContent = card.getSuit();
-  
-    valueContainer.append(value);
-    valueContainer.append(suit);
-    playingCard.append(valueContainer);
-  
-    const table = document.querySelector(".table");
-    table.append(playingCard);
-  }
